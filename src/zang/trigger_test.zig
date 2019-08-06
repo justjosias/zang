832d7b35844a8796db52bcecfc38f3e5d727fd12
dbandstra <dbandstra@protonmail.com>
2019-08-05 23:09:54 -0700

    write tests for Trigger

---
+++ src/zang/trigger_test.zig
@@ -0,0 +1,128 @@
+const std = @import("std");
+
+const Trigger = @import("trigger.zig").Trigger;
+const Impulse = @import("notes.zig").Impulse;
+const Notes = @import("notes.zig").Notes;
+const Span = @import("basics.zig").Span;
+
+const span = Span {
+    .start = 0,
+    .end = 1024,
+};
+
+const ExpectedResult = struct {
+    start: usize,
+    end: usize,
+    params: f32,
+    note_id_changed: bool,
+};
+
+fn testAll(trigger: *Trigger(f32), iap: Notes(f32).ImpulsesAndParamses, expected: []const ExpectedResult) void {
+    var ctr = trigger.counter(span, iap);
+
+    for (expected) |e| {
+        const r = trigger.next(&ctr).?;
+        std.testing.expectEqual(e.start, r.span.start);
+        std.testing.expectEqual(e.end, r.span.end);
+        std.testing.expectEqual(e.params, r.params);
+        std.testing.expectEqual(e.note_id_changed, r.note_id_changed);
+    }
+
+    std.testing.expectEqual((?Trigger(f32).NewPaintReturnValue)(null), trigger.next(&ctr));
+}
+
+test "Trigger: no notes" {
+    var trigger = Trigger(f32).init();
+
+    testAll(&trigger, Notes(f32).ImpulsesAndParamses {
+        .impulses = [_]Impulse {},
+        .paramses = [_]f32 {},
+    }, [_]ExpectedResult {});
+}
+
+test "Trigger: first note at frame=0" {
+    var trigger = Trigger(f32).init();
+
+    testAll(&trigger, Notes(f32).ImpulsesAndParamses {
+        .impulses = [_]Impulse {
+            Impulse { .frame = 0, .note_id = 1 },
+        },
+        .paramses = [_]f32 {
+            440.0,
+        },
+    }, [_]ExpectedResult {
+        ExpectedResult { .start = 0, .end = 1024, .params = 440.0, .note_id_changed = true },
+    });
+}
+
+test "Trigger: first note after frame=0" {
+    var trigger = Trigger(f32).init();
+
+    testAll(&trigger, Notes(f32).ImpulsesAndParamses {
+        .impulses = [_]Impulse {
+            Impulse { .frame = 500, .note_id = 1 },
+        },
+        .paramses = [_]f32 {
+            440.0,
+        },
+    }, [_]ExpectedResult {
+        ExpectedResult { .start = 500, .end = 1024, .params = 440.0, .note_id_changed = true },
+    });
+}
+
+test "Trigger: carryover" {
+    var trigger = Trigger(f32).init();
+
+    testAll(&trigger, Notes(f32).ImpulsesAndParamses {
+        .impulses = [_]Impulse {
+            Impulse { .frame =   0, .note_id = 1 },
+            Impulse { .frame = 200, .note_id = 2 },
+        },
+        .paramses = [_]f32 {
+            440.0,
+            220.0,
+        },
+    }, [_]ExpectedResult {
+        ExpectedResult { .start =   0, .end =  200, .params = 440.0, .note_id_changed = true },
+        ExpectedResult { .start = 200, .end = 1024, .params = 220.0, .note_id_changed = true },
+    });
+
+    testAll(&trigger, Notes(f32).ImpulsesAndParamses {
+        .impulses = [_]Impulse {
+            Impulse { .frame = 500, .note_id = 3 },
+            Impulse { .frame = 600, .note_id = 3 }, // same
+        },
+        .paramses = [_]f32 {
+            330.0,
+            660.0,
+        },
+    }, [_]ExpectedResult {
+        ExpectedResult { .start =   0, .end =  500, .params = 220.0, .note_id_changed = false },
+        ExpectedResult { .start = 500, .end =  600, .params = 330.0, .note_id_changed = true },
+        ExpectedResult { .start = 600, .end = 1024, .params = 660.0, .note_id_changed = false },
+    });
+
+    testAll(&trigger, Notes(f32).ImpulsesAndParamses {
+        .impulses = [_]Impulse {},
+        .paramses = [_]f32 {},
+    }, [_]ExpectedResult {
+        ExpectedResult { .start = 0, .end = 1024, .params = 660.0, .note_id_changed = false },
+    });
+}
+
+test "Trigger: two notes starting at the same time" {
+    var trigger = Trigger(f32).init();
+
+    testAll(&trigger, Notes(f32).ImpulsesAndParamses {
+        .impulses = [_]Impulse {
+            Impulse { .frame = 200, .note_id = 1 },
+            Impulse { .frame = 200, .note_id = 2 },
+        },
+        .paramses = [_]f32 {
+            440.0,
+            220.0,
+        },
+    }, [_]ExpectedResult {
+        ExpectedResult { .start = 200, .end = 1024, .params = 220.0, .note_id_changed = true },
+    });
+}
