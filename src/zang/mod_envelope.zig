dc764808138f2d59fc124654b6e8dceed1c1ee77
dbandstra <dbandstra@protonmail.com>
2019-08-19 19:37:31 -0700

    ADSR envelope now uses non-linear falloff function
    hardcoded to 'x cubed' curve for now. it sounds a lot better than linear.
    
    i might remove paint_line.zig later

--- src/zang/mod_envelope.zig
+++ src/zang/mod_envelope.zig
@@ -1,5 +1,11 @@
-const paintLineTowards = @import("paint_line.zig").paintLineTowards;
 const Span = @import("basics.zig").Span;
+
+// TODO - make this configurable. maybe just a few stock options (linear,
+// square, cube?)
+fn powerFunc(v: f32) f32 {
+    const iv = 1.0 - v;
+    return 1.0 - iv * iv * iv;
+}
 
 pub const Envelope = struct {
     pub const num_outputs = 1;
@@ -23,66 +29,98 @@ pub const Envelope = struct {
         Sustain,
     };
 
-    envelope: f32,
+    t: f32,
+    last_value: f32,
+    start: f32,
+    goal: f32,
+    duration: f32,
     state: State,
 
     pub fn init() Envelope {
         return Envelope {
-            .envelope = 0.0,
+            .t = 0.0,
+            .last_value = 0.0,
+            .start = 0.0,
+            .goal = 0.0,
+            .duration = 0.0,
             .state = .Idle,
         };
     }
 
-    fn paintOn(self: *Envelope, buf: []f32, params: Params) void {
-        const buf_len = @intCast(u31, buf.len);
-        var i: u31 = 0;
+    fn changeState(self: *Envelope, new_state: State, goal: f32, duration: f32) void {
+        self.state = new_state;
+        self.start = self.last_value;
+        self.goal = goal;
+        self.t = 0.0;
+        self.duration = duration;
+    }
 
-        if (self.state == .Idle) {
-            self.state = .Attack;
+    fn paintTowardGoal(self: *Envelope, buf: []f32, i_ptr: *usize, params: Params) bool {
+        var i = i_ptr.*; defer i_ptr.* = i;
+
+        const t_step = 1.0 / (self.duration * params.sample_rate);
+        var finished = false;
+
+        while (!finished and i < buf.len) : (i += 1) {
+            self.t += t_step;
+            if (self.t >= 1.0) {
+                self.t = 1.0;
+                finished = true;
+            }
+            buf[i] += self.start + powerFunc(self.t) * (self.goal - self.start);
+        }
+
+        self.last_value = self.start + powerFunc(self.t) * (self.goal - self.start);
+
+        return finished;
+    }
+
+    fn paintOn(self: *Envelope, buf: []f32, params: Params, new_note: bool) void {
+        var i: usize = 0;
+
+        if (self.state == .Idle or new_note) {
+            self.changeState(.Attack, 1.0, params.attack_duration);
         }
 
         if (self.state == .Attack) {
-            const goal = 1.0;
-
-            if (self.envelope >= goal) {
-                self.state = .Decay;
-            } else {
-                if (paintLineTowards(&self.envelope, params.sample_rate, buf, &i, params.attack_duration, goal)) {
-                    self.state = .Decay;
+            if (self.paintTowardGoal(buf, &i, params)) {
+                if (params.sustain_volume < 1.0) {
+                    self.changeState(.Decay, params.sustain_volume, params.decay_duration);
+                } else {
+                    self.changeState(.Sustain, 1.0, 0.0);
                 }
             }
 
-            if (i == buf_len) {
+            if (i == buf.len) {
                 return;
             }
         }
 
         if (self.state == .Decay) {
-            const goal = params.sustain_volume;
-
-            if (paintLineTowards(&self.envelope, params.sample_rate, buf, &i, params.decay_duration, goal)) {
-                self.state = .Sustain;
+            if (self.paintTowardGoal(buf, &i, params)) {
+                self.changeState(.Sustain, 1.0, 0.0);
             }
 
-            if (i == buf_len) {
+            if (i == buf.len) {
                 return;
             }
         }
 
         if (self.state == .Sustain) {
-            while (i < buf_len) : (i += 1) {
-                buf[i] += self.envelope;
+            while (i < buf.len) : (i += 1) {
+                buf[i] += self.last_value;
             }
         }
     }
 
     fn paintOff(self: *Envelope, buf: []f32, params: Params) void {
-        self.state = .Idle;
+        if (self.state != .Idle) {
+            self.changeState(.Idle, 0.0, params.release_duration);
+        }
 
-        if (self.envelope > 0.0) {
-            var i: u31 = 0;
-
-            _ = paintLineTowards(&self.envelope, params.sample_rate, buf, &i, params.release_duration, 0.0);
+        if (self.t < 1.0) {
+            var i: usize = 0;
+            _ = self.paintTowardGoal(buf, &i, params);
         }
     }
 
@@ -90,10 +128,7 @@ pub const Envelope = struct {
         const output = outputs[0][span.start..span.end];
 
         if (params.note_on) {
-            if (note_id_changed) {
-                self.state = .Idle;
-            }
-            self.paintOn(output, params);
+            self.paintOn(output, params, note_id_changed);
         } else {
             self.paintOff(output, params);
         }
